14fe17b451af8ddf829bb32b0ac57bb70ee1187a
Thalita Pereira <105465339+pereirathalita@users.noreply.github.com>
2022-08-16 17:01:28 -0300

    feat:  resolvendo exercicios

---
+++ index.ts
@@ -0,0 +1,23 @@
+// ex 1
+
+const primeiroNome: string = "seu nome";
+const diaDoMes: number = 12;
+const hojeFezSol: boolean = true;
+
+// ex 2
+
+type Cachorrro = {
+    nome: string;
+    idade: number;
+    raça: string;
+    grandePorte: string;
+    donos: string;
+  }
+  
+  interface Animais {
+    tipo: string;
+    bipede: boolean;
+    asas: boolean;
+    região: string; 
+  }
+
